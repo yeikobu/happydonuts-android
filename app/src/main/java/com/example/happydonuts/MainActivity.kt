295d6108ab96601bc1a91814651ac10df7927b08
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-04-12 19:45:12 -0400

    Add feature to change top view layout for login and register

--- app/src/main/java/com/example/happydonuts/MainActivity.kt
+++ app/src/main/java/com/example/happydonuts/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.happydonuts
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var topView: View
     private lateinit var loginAndRegisterTabLayout: TabLayout
     private lateinit var loginAndRegisterViewPager2: ViewPager2
     private lateinit var loginFragmentAdapter: LoginFragmentAdapter
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        topView = findViewById(R.id.topView)
         loginAndRegisterTabLayout = findViewById(R.id.tabLayout)
         loginAndRegisterViewPager2 = findViewById(R.id.viewPager2)
         loginFragmentAdapter = LoginFragmentAdapter(supportFragmentManager, lifecycle)
@@ -46,8 +51,10 @@ class MainActivity : AppCompatActivity() {
                 loginAndRegisterTabLayout.selectTab(loginAndRegisterTabLayout.getTabAt(position))
                 if (position == 0) {
                     loginAndRegisterTabLayout.setSelectedTabIndicator(R.drawable.login_tab_indicator)
+                    topView.setBackgroundColor(getColor(R.color.pink))
                 } else {
                     loginAndRegisterTabLayout.setSelectedTabIndicator(R.drawable.register_tab_indicator)
+                    topView.setBackgroundColor(getColor(R.color.green))
                 }
             }
         })
