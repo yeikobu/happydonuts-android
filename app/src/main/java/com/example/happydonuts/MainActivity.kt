b475ae9fa69d70dc404f2b4e18d2c5d40df35676
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-04-12 14:37:46 -0400

    Add feature to change the selected tab indicator dynamicly in the main activity

--- app/src/main/java/com/example/happydonuts/MainActivity.kt
+++ app/src/main/java/com/example/happydonuts/MainActivity.kt
@@ -3,12 +3,8 @@ package com.example.happydonuts
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.viewpager2.widget.ViewPager2
-import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 
 class MainActivity : AppCompatActivity() {
     private lateinit var loginAndRegisterTabLayout: TabLayout
@@ -25,6 +21,7 @@ class MainActivity : AppCompatActivity() {
 
 //        loginAndRegisterTabLayout.addTab(loginAndRegisterTabLayout.newTab().setText("Login"))
 //        loginAndRegisterTabLayout.addTab(loginAndRegisterTabLayout.newTab().setText("Registro"))
+        loginAndRegisterTabLayout
 
         loginAndRegisterViewPager2.adapter = loginFragmentAdapter
 
@@ -47,6 +44,11 @@ class MainActivity : AppCompatActivity() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
                 loginAndRegisterTabLayout.selectTab(loginAndRegisterTabLayout.getTabAt(position))
+                if (position == 0) {
+                    loginAndRegisterTabLayout.setSelectedTabIndicator(R.drawable.login_tab_indicator)
+                } else {
+                    loginAndRegisterTabLayout.setSelectedTabIndicator(R.drawable.register_tab_indicator)
+                }
             }
         })
     }
